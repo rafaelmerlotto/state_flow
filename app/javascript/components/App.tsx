7d831e4f6fc5d40035a2491b2ba4dcacb74ec8d8
root <rafaelmerlotto@gmail.com>
2025-03-20 16:57:38 +0100

    initial commit

---
+++ app/javascript/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+
+
+export default function App() {
+    return (
+        <div>
+            <h1>Ciao</h1>
+
+        </div>
+    )
+}
---
+++ config/importmap.rb
@@ -0,0 +1,2 @@
+pin "react", to: "https://ga.jspm.io/npm:react@18/umd/react.production.min.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18/umd/react-dom.production.min.js"
